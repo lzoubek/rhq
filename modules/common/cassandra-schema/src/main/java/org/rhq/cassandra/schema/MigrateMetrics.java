9f2815d739b5af0fe1114f3ac0f645923b3399d4
John Sanda <jsanda@redhat.com>
2015-02-23 13:18:47 -0500

    add tests driver program and sample data set for schedule ids

---
+++ modules/common/cassandra-schema/src/main/java/org/rhq/cassandra/schema/MigrateMetrics.java
@@ -0,0 +1,46 @@
+package org.rhq.cassandra.schema;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author John Sanda
+ */
+public class MigrateMetrics {
+
+    private static final Log log = LogFactory.getLog(MigrateMetrics.class);
+
+    public static void main(String[] args) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        try {
+            log.info(System.getProperty("rhq.storage.nodes"));
+
+            Properties properties = new Properties();
+            properties.put(SchemaManager.RELATIONAL_DB_CONNECTION_FACTORY_PROP, new DBConnectionFactory() {
+                @Override
+                public Connection newConnection() throws SQLException {
+                    return null;
+                }
+            });
+            properties.put(SchemaManager.DATA_DIR, "target");
+            String[] nodes = System.getProperty("rhq.storage.nodes").split(",");
+            SchemaManager schemaManager = new SchemaManager("rhqadmin", "1eeb2f255e832171df8592078de921bc", nodes,
+                9042);
+            schemaManager.install(properties);
+            schemaManager.shutdown();
+        } catch (Exception e) {
+            log.warn("Schema installation failed", e);
+        } finally {
+            stopwatch.stop();
+            log.info("Finished schema upgrade in " + stopwatch.elapsed(TimeUnit.MINUTES) + " minutes");
+        }
+    }
+    
+}
